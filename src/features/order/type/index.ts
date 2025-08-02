fd73369b843147d62739f36ce7891ad67417150b
Sepide Kia <sepidekia6990@gmail.com>
2025-08-02 19:45:14 +0330

    feat: add the "all" filter to filters, handle the filter with searchParams

--- src/features/order/type/index.ts
+++ src/features/order/type/index.ts
@@ -1,15 +1,17 @@
 export type Order = {
   id: string;
   title: string;
-  type: OrderStatus;
+  type: OrderType;
   startDate: string;
   commandType: 'DRUGS' | 'LAB_TEST' | 'RADIOLOGY';
   metaData: string;
 };
 
-export type OrderStatus = 'duringOrder' | 'outOfOrder';
+export type OrderType = 'duringOrder' | 'outOfOrder';
 
 export type ParsedMetadata = {
   instruction?: string;
   discontinueUsingReason?: string;
 };
+
+export type Filters = 'all' | OrderType;
