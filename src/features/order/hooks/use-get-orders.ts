f6161c2d8bb1d1f4ed38c14dfbaf4be7521dd9f0
Sepide Kia <sepidekia6990@gmail.com>
2025-08-03 07:42:24 +0330

    refactor: separate the concern in use-get-order

--- src/features/order/hooks/use-get-orders.ts
+++ src/features/order/hooks/use-get-orders.ts
@@ -1,16 +1,11 @@
-import { useSearchStore } from '@/store/search-store';
 import { useQuery } from '@tanstack/react-query';
 import { getOrders } from '../service';
 
 export const getOrdersKey = () => ['orders'];
 
 export function useGetOrders() {
-  const debouncedSearch = useSearchStore((s) => s.debouncedSearch);
   return useQuery({
     queryKey: getOrdersKey(),
     queryFn: getOrders,
-    select: (orders) => {
-      return orders.filter((order) => order.title.includes(debouncedSearch));
-    },
   });
 }
